129fc0442bd78edb809ee056b4ecbaa12d940e8f
Ariel <ariel.carrasco.s@mail.pucv.cl>
2024-09-05 16:53:55 -0400

    Función asignarVotantes creada. Se debe probar en main, por lo que podrá sufrir cambios a futuro.

--- src/main/java/com/mycompany/mainproyecto/Comuna.java
+++ src/main/java/com/mycompany/mainproyecto/Comuna.java
@@ -7,7 +7,7 @@ public class Comuna {
     private String nombre;
     private List<LocalDeSufragio> locales;
     
-    public Comuna(int id, String nombre){
+    public Comuna(String nombre){
         this.nombre = nombre;
         this.locales = new ArrayList<>();
     }
@@ -26,5 +26,31 @@ public class Comuna {
     
     public List<LocalDeSufragio> obtenerLocales(){
         return locales;
+    }
+    
+    public void asignarVotantes(List<Votante> votantes) {
+        for (Votante votante : votantes) {
+            String comunaVotante = votante.getComuna();
+
+            if (comunaVotante.equals(this.nombre)) {
+                boolean asignacion = false;
+                
+                for (LocalDeSufragio local : locales) {
+                    if (local.verificarCapacidad()) {
+                        local.agregarVotante(votante);
+                        System.out.println("Votante " + votante.getNombre() + " asignado al local " + local.getNombre());
+                        asignacion = true;
+                        break;
+                    }
+                }
+                
+                if (!asignacion) {
+                    System.out.println("No se puede agregar al votante " + votante.getNombre() + " en la comuna " + comunaVotante + " porque todos los locales están llenos.");
+                }
+                return;
+            }
+        }
+        // Si no se encontró la comuna del votante en la lista de locales
+        System.out.println("Comuna " + comunaVotante + " no encontrada en los locales de esta comuna.");
     }
 }
